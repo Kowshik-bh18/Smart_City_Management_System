7bfbbb57c9287e437eecb1f3470ccda491a57624
MDGanesha <ganeshamd24@gmail.com>
2025-04-30 08:42:22 +0530

    final-commit-last

--- emechatbot/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ChatForm(forms.Form):
-    message = forms.CharField(label='You', widget=forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Type your emergency message...',
-    }))
